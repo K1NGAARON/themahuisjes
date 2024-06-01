455a9d9de8f389318548623c2ff73dbf5eb561a0
K1NGAARON <gerardaaron1997@gmail.com>
2024-06-01 13:36:56 +0200

    cleaned css script

--- huisjes/js/script.js
+++ huisjes/js/script.js
@@ -1,13 +1,13 @@
 $(window).scroll(function() {
-    var scrollTop = $(document).scrollTop();
-    var viewportHeight = $(window).height();
-    var sections = $('.content');
-    var numSections = sections.length;
+    let scrollTop = $(document).scrollTop();
+    let viewportHeight = $(window).height();
+    let sections = $('.content');
+    let numSections = sections.length;
 
     sections.each(function(index) {
-        var sectionOffsetTop = $(this).offset().top;
-        var distanceFromTop = scrollTop - sectionOffsetTop + viewportHeight / 2;
-        var opacity;
+        let sectionOffsetTop = $(this).offset().top;
+        let distanceFromTop = scrollTop - sectionOffsetTop + viewportHeight / 2;
+        let opacity;
 
         if (distanceFromTop >= 0 && distanceFromTop <= viewportHeight) {
             opacity = Math.min(1, (index + 1) / numSections * 0.85 + 0.15); // Increase opacity from 0.15 to 1
