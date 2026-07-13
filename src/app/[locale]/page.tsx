c1b7ea890d1e3a7fd1e7afc188bafc78b31b5064
Kitana Vanhoucke <gerardaaron1997@gmail.com>
2026-07-13 20:54:16 +0200

    coming soon page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,6 @@
 import { getTranslations, setRequestLocale } from "next-intl/server";
-import { Link } from "@/i18n/navigation";
-import AnimateOnScroll from "@/components/AnimateOnScroll";
 import Footer from "@/components/Footer";
-import {
-  comingSoonHuisjes,
-  getHuisjeBannerImage,
-  huisjes,
-} from "@/data/huisjes";
-import "@/styles/home.css";
+import "@/styles/coming-soon.css";
 
 export default async function HomePage({
   params,
@@ -21,46 +14,12 @@ export default async function HomePage({
   const tc = await getTranslations("common");
 
   return (
-    <div className="section banner">
-      <AnimateOnScroll className="row flex-col center">
-        <h1>
-          {t("welcome")}
-          <br />
-          <span className="focus-heading">{t("focusHeading")}</span>
-        </h1>
-        <div className="projects-wrapper">
-          {huisjes.map((huisje) => (
-            <Link
-              key={huisje.slug}
-              href={`/huisjes/${huisje.slug}`}
-              className={`item ${huisje.themeClass}`}
-            >
-              <img
-                src={getHuisjeBannerImage(huisje.slug)}
-                alt={huisje.name}
-              />
-              <div className="content">
-                <p className="city">{huisje.city}</p>
-                <h3 className="notranslate">{huisje.name}</h3>
-                <span className="more-info">{tc("moreInfo")}</span>
-              </div>
-              <div className="overlay" />
-            </Link>
-          ))}
-          {comingSoonHuisjes.map((huisje) => (
-            <div key={huisje.slug} className="item coming-soon">
-              <img src={huisje.frontImage} alt={huisje.name} />
-              <div className="coming-soon-content">
-                {huisje.city ? <p className="city">{huisje.city}</p> : null}
-                <h3 className="notranslate">{huisje.name}</h3>
-                <span className="badge">{tc("comingSoon")}</span>
-              </div>
-              <div className="coming-soon-overlay" />
-            </div>
-          ))}
-          <div className="item large" />
-        </div>
-      </AnimateOnScroll>
+    <div className="coming-soon-page">
+      <main className="coming-soon-main">
+        <span className="badge">{tc("comingSoon")}</span>
+        <h1>{tc("siteName")}</h1>
+        <p>{t("comingSoonMessage")}</p>
+      </main>
       <Footer />
     </div>
   );
